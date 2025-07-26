5846957bf9034679d16a325d7836b874b3071482
Geethanjali009 <geethanjali2984@gmail.com>
2025-07-25 18:45:33 -0700

    Add files via upload

--- Executor.java
+++ Executor.java
@@ -1,36 +1,25 @@
-package FirstPackage;
+package SecondPackage;
 
 
-	public class Executor {
-		public static void main(String[] args) {
-			
-		//accessing same package class
-			Base b1=new Base();
-			b1.methodDefault();
-			b1.methodProtected();
-			b1.methodPublic();
-			
-			b1.varDefault=11;
-			b1.methodDefault();
-			
-			//private member can't accessible
-			/*b1.varPrivate=21; 
-			b1.methodPrivate();*/
-			
-			b1.varProtected=31;
-			b1.methodProtected();
-			
-			b1.varPublic=41;
-			b1.methodPublic();
-			
-			//instance of operator
-			System.out.println(b1 instanceof Base); //returns True
-			
-			/*
-			 * Person p=new Person(); System.out.println(p instanceof Base); //CTE
-			 * Incompatible types
-			 */	}
+import FirstPackage.Base;
+public class Executor {
+	public static void main(String[] args) {
+		// accessing different package class
+		Base b1 = new Base();
 
+		// private, default and protected members can't access
+
+		/*
+		 * b1.varPrivate=21; b1.methodPrivate(); b1.varDefault=11; b1.methodDefault();
+		 * b1.varProtected=31; b1.methodProtected();
+		 */
+
+		b1.methodPublic();
+		b1.varPublic = 41;
+		b1.methodPublic();
 	}
+}
+	
+	
 
 
