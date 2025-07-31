321bbce6962c1886e48118189bec7bcb0c230897
Geethanjali009 <geethanjali2984@gmail.com>
2025-07-31 08:23:07 -0700

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,25 +1,20 @@
-package day7;
+package day10;
 
-class Student implements Cloneable {
-	int rollNo;
-	String className;
-	String name;
+public class Student {
+	private int rollNo;
+	private String name;
 
-	// Getters and setters
+	Student(int rollNo, String name) {
+		this.rollNo = rollNo;
+		this.name = name;
+	}
+
 	public int getRollNo() {
 		return rollNo;
 	}
 
 	public void setRollNo(int rollNo) {
 		this.rollNo = rollNo;
-	}
-
-	public String getClassName() {
-		return className;
-	}
-
-	public void setClassName(String className) {
-		this.className = className;
 	}
 
 	public String getName() {
@@ -29,31 +24,4 @@ class Student implements Cloneable {
 	public void setName(String name) {
 		this.name = name;
 	}
-
-	// Class constructor
-	public Student(int rollNo, String className, String name) {
-		this.rollNo = rollNo;
-		this.className = className;
-		this.name = name;
-	}
-
-	// Print method
-	public void printData() {
-		System.out.println("Name : " + name + ", RollNo: " + rollNo + ", Class Name : " + className);
-	}
-
-	// Override the clone method
-
-	@Override
-	public Object clone() throws CloneNotSupportedException {
-		return super.clone();
-	}
-
-	// Override the clone method - Covariant overriding
-
-	/*
-	 * @Override public Student clone() throws CloneNotSupportedException { return
-	 * (Student) super.clone(); }
-	 */
-
 }
