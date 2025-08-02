b714f5edf10433f8f980d5408bd0efa45421270f
Geethanjali009 <geethanjali2984@gmail.com>
2025-08-01 21:46:01 -0700

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,34 +1,65 @@
-package day17;
+package day18;
 
-public class Student {
-	int marks;
-	String name;
-	// Constructor
-	public Student(String name, int marks)
-	{
-		this.marks = marks;
+public class Student implements Comparable 
+{
+	private int rollNo;
+	private String name;
+	private float per;
+
+	public Student(int rollNo, String name, float per) {
+		super();
+		this.rollNo = rollNo;
+		this.name = name;
+		this.per = per;
+	}
+
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getRollNo() {
+		return rollNo;
+	}
+
+	public void setRollNo(int rollNo) {
+		this.rollNo = rollNo;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
 		this.name = name;
 	}
-	// Getters and Setters
-	public int getMarks() { return marks; }
-	public String getName() { return name; }
-	public void setName(String name) { this.name = name; }
-	public void setMarks(int marks) { this.marks = marks; }
-	
-	@Override public int hashCode()
-	{
+
+	public float getPer() {
+		return per;
+	}
+
+	public void setPer(float per) {
+		this.per = per;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [rollNo=" + rollNo + ", name=" + name + ", per=" + per + "]";
+	}
+
+	@Override
+	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + marks;
-		result = prime * result
-				+ ((name == null) ? 0 : name.hashCode());
+
+		result = prime * result * (int) per + ((name == null) ? 0 : name.hashCode());
 		return result;
 	}
+
 	@Override
 	// if both the object references are
 	// referring to the same object.
-	public boolean equals(Object obj)
-	{
+	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
 		if (obj == null)
@@ -37,23 +68,24 @@ public class Student {
 			return false;
 
 		// type casting of the argument.
-		Student other = (Student)obj;
+		Student other = (Student) obj;
 
 		// comparing the state of argument with
 		// the state of 'this' Object
-		if (marks != other.marks)
+		if (rollNo != other.rollNo)
 			return false;
 		if (name == null) {
 			if (other.name != null)
 				return false;
-		}
-		else if (!name.equals(other.name))
+		} else if (!name.equals(other.name))
 			return false;
 		return true;
 	}
-	@Override
-	public String toString() {
-		return "Student [marks=" + marks + ", name=" + name + "]";
+
+	//@Override
+	public int compareTo(Object o) {
+		Student s2 = (Student) o;
+		//return (int) (this.rollNo - s2.rollNo);
+		return this.name.compareTo(s2.name);
 	}
-	
 }
